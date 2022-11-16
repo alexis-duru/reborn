bead0986d449cf97cd5d2adae85911f6f7dbff3f
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-11-16 14:55:15 +0100

    update

---
+++ components/ScrollGallery.js
@@ -0,0 +1,46 @@
+const ScrollGallery = () => {
+
+    return (
+        <>
+            <section id="scrollGallery">
+                <div class="slider">
+                    <div class="slider-inner">
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="wrapper">
+                                <div class="img"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+
+}
+
+export default ScrollGallery;
