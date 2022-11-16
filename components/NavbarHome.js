ac4cae8bc60e985b6bfd7b1b9502855867825855
Corentin Corre <corentin.corre@hotmail.fr>
2022-11-16 14:54:36 +0100

    nav bar home fix

--- components/NavbarHome.js
+++ components/NavbarHome.js
@@ -1,8 +1,24 @@
 import Image from 'next/image'
 import logo from '../public/assets/img/logo-reborn.svg'
 import panier from '../public/assets/icons/reborn-panier.svg'
+import { useEffect } from "react";
 
 export default function Header() {
+    function bg() {
+        const header = document.querySelector('.navbar__home');
+        window.addEventListener('scroll', () => {
+            let scroll = window.scrollY;
+            if (scroll > 200) {
+                header.classList.add('bg')
+            }
+            else {
+                header.classList.remove('bg')
+            }
+        })
+    }
+    useEffect(() => {
+        bg();
+    }, [])
     return (
         <>
             <section className='navbar__home'>
