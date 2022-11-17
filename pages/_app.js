d43982f902ca5e08dc8cbf0995a0ba84b9e8a5dd
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-11-17 12:33:23 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,12 @@ function MyApp({ title, Component, pageProps }) {
 
   return (
     <>
-      <Head>
+          <Head>
         <title>Reborn - donner une seconde vie à vos objets</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Reborn - donner une seconde vie à vos objets" key="title" />
+        <meta property="og:description" content="Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf" />
+        <meta name='description' content='Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf' />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -29,13 +29,6 @@ export default function Home() {
   }, [])
   return (
     <>
-      <Head>
-        <title>Reborn - donner une seconde vie à vos objets</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Reborn - donner une seconde vie à vos objets" key="title" />
-        <meta property="og:description" content="Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf" />
-        <meta name='description' content='Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf' />
-      </Head>
       <Layout>
         <Cursor />
         <Splashscreen />
