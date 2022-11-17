8b3be8f112eb4ea6a725a2db1b7a0331c7357d0f
Corentin Corre <corentin.corre@hotmail.fr>
2022-11-17 12:41:33 +0100

    fix du home

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,9 @@ import Link from 'next/link';
 import FeaturedProducts from '../components/FeaturedProducts';
 import Actualite1 from '../public/assets/img/home/reborn-actualite1.webp'
 import Actualite2 from '../public/assets/img/home/reborn-actualite2.webp'
-import Head from 'next/head'
 
 export default function Home() {
   function parallax() {
-    const body = document.querySelector('body');
-    body.classList.add("home")
     const bg = document.querySelector(".header_bg");
     window.addEventListener("scroll", () => {
       let scroll = window.scrollY;
@@ -29,84 +26,80 @@ export default function Home() {
   }, [])
   return (
     <>
-      <Head>
-        <title>Reborn - donner une seconde vie à vos objets</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Reborn - donner une seconde vie à vos objets" key="title" />
-        <meta property="og:description" content="Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf" />
-        <meta name='description' content='Chez Reborn, nous souhaitons permettre à chacun d’avoir des objets uniques, sans se ruiner et sans acheter du neuf' />
-      </Head>
       <Layout>
         <Cursor />
         <Splashscreen />
         <NavbarHome></NavbarHome>
-        <section className="header">
-          <div className="header__container">
-            <div className="header__content">
-              <Image src={reborn} alt="Logo reborn" className="reborn-logo" loading="eager" width={400} />
-            </div>
-          </div>
-          <div className="header_bg"></div>
-        </section>
-
-        <section className="section-colored homepage__section">
-          <div className="container">
-            <h1>Notre<span className='span-font'> Concept</span></h1>
-            <div className='homepage__content'>
-              <p>Parce qu'on nous met pas à la poubelle quand on se casse une jambe. Reborn retape les objets un peu usées par la vie</p>
-              <div className='btn__container'><Link href="/concept" className="btn primary-btn send-btn">
-                Lire plus
-              </Link></div>
-            </div>
-          </div>
-        </section>
-
-        <section className="section-colored homepage__section">
-          <div className="container">
-            <h1>Les<span className="span-font"> petits nouveaux</span></h1>
-            <div className="homepage__content content">
-              <p>Chez Reborn, nous souhaitons permettre à chacun d'avoir des objets uniques, sans se ruiner et sans acheter du neuf !</p>
-              <p>C'est pour cela que nous vous proposons de réparer vos objets d'une manière singulière ou d'acheter des pièces par nos soins à partir de vos dons.</p>
-            </div>
-            <FeaturedProducts></FeaturedProducts>
-          </div>
-        </section>
-
-
-        <section className="section-colored homepage__section">
-          <div className="container">
-            <h1>Fair un<span className='span-font'> don</span></h1>
-            <div className="dons__content content">
-              <div className="dons__icon__container"> <Image src={donsIcon} alt="Icon de dons"></Image></div>
-              <p>Vous avez sans doute déjà vu nos équipes avec leur vélos cargo sillonner Bordeaux avec le sourire aux lèvres… c’est normal, on est toujours plus étonné par votre générosité ! Du coup, c’est tout simple, soit vous nous déposez les objets dont vous voulez vous débarrasser à notre atelier directement (ici), soit on vient les chercher chez vous, avec nos vélos cargo ou notre super camion électrique (uniquement à Bordeaux).</p>
-              <div className='btn__container'><Link href="/concept" className="btn primary-btn send-btn">
-                Lire plus
-              </Link></div>
-            </div>
-          </div>
-        </section>
-
-        <section className="section-colored homepage__section">
-          <div className="container">
-            <h1>Les<span className="span-font"> actus</span></h1>
-            <div className="homepage__content content">
-              <p>Vous avez sans doute déjà vu nos équipes avec leur vélos cargo sillonner Bordeaux avec le sourire aux lèvres… c’est normal, on est toujours plus étonné par votre générosité ! Du coup, c’est tout simple, soit vous nous déposez les objets dont vous voulez vous débarrasser à notre atelier directement (ici), soit on vient les chercher chez vous, avec nos vélos cargo ou notre super camion électrique (uniquement à Bordeaux).</p>
-            </div>
-            <div className='actus__container'>
-              <div className='actu__container'>
-                <Image src={Actualite1} alt="Photographie brocante des Quinquonces"></Image>
-                <p className='actu__content'>Les 25 et 26 novembre, c’est la brocante des Quinquonces !
-                  L’équipe Reborn aura un stand autour duquel on pourra discuter et refaire le monde autour de nos toutes dernières créations. Vous aurez aussi la possibilité d’y réparer et personnaliser vos objets fraîchement chinés dans une ambiance conviviale et créative. On vous attend de pied ferme !
-                </p>
-              </div>
-              <div className='actu__container'>
-                <Image src={Actualite2} alt="Photographie plaque atelier reborn"></Image>
-                <p className='actu__content'>Les 3 et 4 décembre, c'est la fête chez Reborn ! On vous ouvre nos portes pour que vous puissiez découvrir les métiers associés à la réparation et échanger avec nos équipes sur leur rôle au sein de l'entreprise. On vous attend nombreux !
-                </p>
+        <div className='home'>
+          <section className="header">
+            <div className="header__container">
+              <div className="header__content">
+                <Image src={reborn} alt="Logo reborn" className="reborn-logo" loading="eager" width={400} />
               </div>
             </div>
-          </div>
-        </section>
+            <div className="header_bg"></div>
+          </section>
+
+          <section className="section-colored homepage__section">
+            <div className="container">
+              <h1>Notre<span className='span-font'> Concept</span></h1>
+              <div className='homepage__content'>
+                <p>Parce qu'on nous met pas à la poubelle quand on se casse une jambe. Reborn retape les objets un peu usées par la vie</p>
+                <div className='btn__container'><Link href="/concept" className="btn primary-btn send-btn">
+                  Lire plus
+                </Link></div>
+              </div>
+            </div>
+          </section>
+
+          <section className="section-colored homepage__section">
+            <div className="container">
+              <h1>Les<span className="span-font"> petits nouveaux</span></h1>
+              <div className="homepage__content content">
+                <p>Chez Reborn, nous souhaitons permettre à chacun d'avoir des objets uniques, sans se ruiner et sans acheter du neuf !</p>
+                <p>C'est pour cela que nous vous proposons de réparer vos objets d'une manière singulière ou d'acheter des pièces par nos soins à partir de vos dons.</p>
+              </div>
+              <FeaturedProducts></FeaturedProducts>
+            </div>
+          </section>
+
+
+          <section className="section-colored homepage__section">
+            <div className="container">
+              <h1>Fair un<span className='span-font'> don</span></h1>
+              <div className="dons__content content">
+                <div className="dons__icon__container"> <Image src={donsIcon} alt="Icon de dons"></Image></div>
+                <p>Vous avez sans doute déjà vu nos équipes avec leur vélos cargo sillonner Bordeaux avec le sourire aux lèvres… c’est normal, on est toujours plus étonné par votre générosité ! Du coup, c’est tout simple, soit vous nous déposez les objets dont vous voulez vous débarrasser à notre atelier directement (ici), soit on vient les chercher chez vous, avec nos vélos cargo ou notre super camion électrique (uniquement à Bordeaux).</p>
+                <div className='btn__container'><Link href="/concept" className="btn primary-btn send-btn">
+                  Lire plus
+                </Link></div>
+              </div>
+            </div>
+          </section>
+
+          <section className="section-colored homepage__section">
+            <div className="container">
+              <h1>Les<span className="span-font"> actus</span></h1>
+              <div className="homepage__content content">
+                <p>Vous avez sans doute déjà vu nos équipes avec leur vélos cargo sillonner Bordeaux avec le sourire aux lèvres… c’est normal, on est toujours plus étonné par votre générosité ! Du coup, c’est tout simple, soit vous nous déposez les objets dont vous voulez vous débarrasser à notre atelier directement (ici), soit on vient les chercher chez vous, avec nos vélos cargo ou notre super camion électrique (uniquement à Bordeaux).</p>
+              </div>
+              <div className='actus__container'>
+                <div className='actu__container'>
+                  <Image src={Actualite1} alt="Photographie brocante des Quinquonces"></Image>
+                  <p className='actu__content'>Les 25 et 26 novembre, c’est la brocante des Quinquonces !
+                    L’équipe Reborn aura un stand autour duquel on pourra discuter et refaire le monde autour de nos toutes dernières créations. Vous aurez aussi la possibilité d’y réparer et personnaliser vos objets fraîchement chinés dans une ambiance conviviale et créative. On vous attend de pied ferme !
+                  </p>
+                </div>
+                <div className='actu__container'>
+                  <Image src={Actualite2} alt="Photographie plaque atelier reborn"></Image>
+                  <p className='actu__content'>Les 3 et 4 décembre, c'est la fête chez Reborn ! On vous ouvre nos portes pour que vous puissiez découvrir les métiers associés à la réparation et échanger avec nos équipes sur leur rôle au sein de l'entreprise. On vous attend nombreux !
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </div>
 
         <Footer></Footer>
       </Layout>
