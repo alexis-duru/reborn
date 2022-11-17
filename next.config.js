61308ed89a76f04dc3959e9634951f5400774f07
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-11-17 12:08:56 +0100

    update headers

--- next.config.js
+++ next.config.js
@@ -2,36 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value:
-              "max-age=15552001; includeSubDomains; preload",
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'Permissions-Policy',
-            value: "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), display-capture=(), document-domain=(), encrypted-media=(), geolocation=(), fullscreen=(), gyroscope=(), layout-animations=(), legacy-image-formats=(), magnetometer=(), microphone=(), midi=(), oversized-images=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), wake-lock=(), xr-spatial-tracking=(), notifications=(), speaker=(), vibrate=(), push=()",
-          },
-        ],
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
